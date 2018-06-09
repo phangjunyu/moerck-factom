4c8a4227f36f9ceeda881e1bce64a01ce032a8fb
nathanaelraj <nathanaelraj@hotmail.com>
2018-06-09 13:58:28 -0700

    glossarydjnkqedjwdqedjkbeqkfbvw

--- pb.py
+++ pb.py
@@ -1,4 +1,5 @@
 import registrationFunctions as rf
+import chainFunctions as cf
 import time, secrets
 from glossary import VA, RA
 class Vote:
@@ -9,35 +10,47 @@ class PollingBooth:
     def __init__(self, votingStationID):
         self.votingStationID = votingStationID
 
-    def receiveVote(choice, userID):
-        if validate(userID):
+    def receiveVote(self, choice, userID):
+        if self.validate(userID):
             vote = Vote(choice)
             return vote
         else:
             print('Voter has already voted or has not been registered')
             return False
 
-    def validate(userID):
+    def validate(self, userID):
         return (not rf.checkTokens(VA, userID)) and rf.checkTokens(RA, userID)
 
-    def submitVote(vote):
+    def submitVote(self, vote, userID):
         zzTime = secrets.randbelow(10)
-        time.sleep(zzTime)
-        cf.updateChain({'vote' : vote.choice} ,vote.votingStationID)
-        cf.putVA(user)
+        #time.sleep(zzTime)
+        cf.updateChain({'vote' : vote.choice} , self.votingStationID)
+        rf.putToken(VA, userID)
+        print('voted for', vote.choice)
 
-if __name__ = 'main':
+def checkVoter(name, uid):
+    status = rf.checkVoter(name, uid)
+    voter_ = status['items']
+    if len(voter_) == 0:
+        return False
+    else:
+        return voter_[0]['chain_id']
+
+if __name__ == '__main__':
     #we assume that we have created a voting station by now and have its chain ID
     #when a user enters we first check
+
     votingStationID = 'c4a852f7e5216f315093f7024b6e9f445cbce22e142de3b034b4def1834ff0bd'
     pollingbooth = PollingBooth(votingStationID)
+    voterName = 'JY'
     voterID = 'jcknwjkdn'
 
     #check if user already exist in the voter chain
-    if checkUserExist(voterID):
-        voterChainID = rf.getChainID(voterEX_ID)
-    else:
-        voterChainID = rf.createVoter(voterEX_ID)
+    voterChainID = checkVoter(voterName, voterID)
+    if not voterChainID:
+        status = rf.createVoter(voterName, voterID)
+        voterChainID = status['chain_id']
+
     #put the RA token of the voter
     rf.putToken(RA, voterChainID)
     #once the RA token of the voter has been set, the voter is ready to vote
@@ -46,4 +59,4 @@ if __name__ = 'main':
     if vote is False:
         raise ValueError
     else:
-        pollingbooth.submitVote(vote)
+        pollingbooth.submitVote(vote, voterChainID)
