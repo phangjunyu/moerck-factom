12d31e3f123c079c0ea85980d084e7d1cd5fef1b
swenjun <swenjun22@gmail.com>
2018-06-09 14:44:20 -0500

    test
    dddd

--- chainFunctions.py
+++ chainFunctions.py
@@ -9,6 +9,10 @@ headers = {
     "factom-provider-token": "D5Sf9ibJxKN4NSHRTpx3OTt5vwfpepIWnE5opJTGKVvBDiJ6"
 }
 
+
+def main():
+    print ("In Main")
+
 def createVoteChain(voting_station_id):
 
     b_unixtime = base64.b64encode(str(time.time()).encode('ascii')).decode('UTF-8')
